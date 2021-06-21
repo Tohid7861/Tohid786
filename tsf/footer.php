120174093e963675dc73da862745c732e080ab23
Tohid7861 <86188262+Tohid7861@users.noreply.github.com>
2021-06-21 12:09:39 +0530

    Add files via upload

---
+++ tsf/footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header">© 2021. Developed by Tohid Momin <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
---
+++ tsf/index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1>Welcome to TSF Bank</h1>
+
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">View All Customers</button></a></li>
+        <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Transfer Money</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">View Transaction History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
